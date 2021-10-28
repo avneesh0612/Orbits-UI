92145ed0081ead1018c238fbec6d1d0c00604ce3
Your Name <avneeshagarwal0612@gmail.com>
2021-10-28 17:40:47 +0530

    initialized starter

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1 className="text-4xl font-bold">Let&apos;s build Flux UI</h1>
+    </div>
+  );
+}
