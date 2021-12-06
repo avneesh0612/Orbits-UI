fb74a6c251726624e9ea28bce7f7b32c36d1823b
Anurag <kr.anurag24@gmail.com>
2021-12-06 12:41:17 +0000

    fix: export all the components

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,8 @@ import Modal from "./components/Modal";
 import Input from "./components/Input";
 import Avatar from "./components/Avatar";
 import TagInput from "./components/TagInput";
+import Stack from "./components/Stack";
+import Badge from "./components/Badge";
+import Spinner from "./components/Spinner";
 
-export { Button, Modal, Input, Avatar, TagInput };
+export { Button, Modal, Input, Avatar, TagInput, Stack, Badge, Spinner };
