12676edd73799bc7fc65af46220bac34a0d67d49
Anurag <kr.anurag24@gmail.com>
2021-12-13 13:38:39 +0000

    fix: update styles

--- src/components/FileInput/FileInput.tsx
+++ src/components/FileInput/FileInput.tsx
@@ -15,8 +15,9 @@ const FileInput = ({ className }: FileInputProps) => {
       className={`text-sm text-gray-500
         file:mr-4 file:py-2 file:px-4
         file:rounded-full file:border-0
-        file:text-sm file:bg-violet-50 
-        file:text-violet-700 hover:file:bg-violet-100 ${className}
+        file:text-sm file:bg-violet-50 file:font-medium
+        file:text-violet-700 hover:file:bg-violet-100 
+        file:cursor-pointer  ${className}
       `}
     />
   );
