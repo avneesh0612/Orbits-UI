cce29d01f6388153c286036d7f4d5772f33ae65a
Anurag <kr.anurag24@gmail.com>
2022-01-03 11:03:01 +0000

    feat: add bgColor prop to divider

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -2,11 +2,18 @@ import React, { ReactNode } from "react";
 import "../../../styles/global.css";
 
 export interface DividerProps {
+  backgroundColor?: string;
   className?: string;
 }
 
-const Divider = ({ className }: DividerProps) => {
-  return <div className="w-full h-xm bg-gray-100 rounded-full" aria-label="divider"></div>;
+const Divider = ({ className, backgroundColor }: DividerProps) => {
+  return (
+    <div
+      className={`bg-gray-300 h-xm ${className}`}
+      aria-label="divider"
+      style={backgroundColor ? { backgroundColor } : {}}
+    ></div>
+  );
 };
 
 export default Divider;
